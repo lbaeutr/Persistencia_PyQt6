bee2c67eeeda3e33c2eadd614bb84ce8284900f1
lbaeutr <lbaeutr@gmail.com>
2025-03-04 17:12:26 +0100

    Firebase:luis

--- juego_ui.py
+++ juego_ui.py
@@ -49,13 +49,13 @@ class Ui_MainWindow(object):
         self.login.setStyleSheet(u"QWidget {\n"
 "    background: transparent;\n"
 "}")
-        self.groupBox = QGroupBox(self.login)
-        self.groupBox.setObjectName(u"groupBox")
-        self.groupBox.setGeometry(QRect(380, 210, 431, 281))
+        self.groupBox_login = QGroupBox(self.login)
+        self.groupBox_login.setObjectName(u"groupBox_login")
+        self.groupBox_login.setGeometry(QRect(380, 210, 431, 281))
         font = QFont()
         font.setPointSize(26)
-        self.groupBox.setFont(font)
-        self.groupBox.setStyleSheet(u"QGroupBox {\n"
+        self.groupBox_login.setFont(font)
+        self.groupBox_login.setStyleSheet(u"QGroupBox {\n"
 "    background-color: rgba(255, 255, 255, 0.2); \n"
 "    border: 2px solid white;  \n"
 "    border-radius: 10px;  \n"
@@ -88,25 +88,26 @@ class Ui_MainWindow(object):
 "    background-color: #45a049;\n"
 "}\n"
 "")
-        self.correo_label = QLabel(self.groupBox)
+        self.correo_label = QLabel(self.groupBox_login)
         self.correo_label.setObjectName(u"correo_label")
         self.correo_label.setGeometry(QRect(50, 40, 181, 16))
         font1 = QFont()
+        font1.setPointSize(1)
         font1.setBold(True)
         self.correo_label.setFont(font1)
-        self.contrasena_label = QLabel(self.groupBox)
+        self.contrasena_label = QLabel(self.groupBox_login)
         self.contrasena_label.setObjectName(u"contrasena_label")
         self.contrasena_label.setGeometry(QRect(50, 110, 111, 16))
-        self.input_correo = QLineEdit(self.groupBox)
+        self.input_correo = QLineEdit(self.groupBox_login)
         self.input_correo.setObjectName(u"input_correo")
         self.input_correo.setGeometry(QRect(50, 70, 331, 31))
-        self.input_contrasena = QLineEdit(self.groupBox)
+        self.input_contrasena = QLineEdit(self.groupBox_login)
         self.input_contrasena.setObjectName(u"input_contrasena")
         self.input_contrasena.setGeometry(QRect(50, 140, 331, 31))
-        self.button_login = QPushButton(self.groupBox)
+        self.button_login = QPushButton(self.groupBox_login)
         self.button_login.setObjectName(u"button_login")
         self.button_login.setGeometry(QRect(170, 190, 75, 31))
-        self.registro_enlace = QLabel(self.groupBox)
+        self.registro_enlace = QLabel(self.groupBox_login)
         self.registro_enlace.setObjectName(u"registro_enlace")
         self.registro_enlace.setGeometry(QRect(80, 230, 271, 20))
         self.registro_enlace.setStyleSheet(u"QLabel {\n"
@@ -120,11 +121,13 @@ class Ui_MainWindow(object):
         self.register_2.setStyleSheet(u"QWidget {\n"
 "    background: transparent;\n"
 "}")
-        self.groupBox_2 = QGroupBox(self.register_2)
-        self.groupBox_2.setObjectName(u"groupBox_2")
-        self.groupBox_2.setGeometry(QRect(380, 170, 431, 341))
-        self.groupBox_2.setFont(font)
-        self.groupBox_2.setStyleSheet(u"QGroupBox {\n"
+        self.groupBox_register = QGroupBox(self.register_2)
+        self.groupBox_register.setObjectName(u"groupBox_register")
+        self.groupBox_register.setGeometry(QRect(380, 170, 431, 341))
+        font2 = QFont()
+        font2.setPointSize(1)
+        self.groupBox_register.setFont(font2)
+        self.groupBox_register.setStyleSheet(u"QGroupBox {\n"
 "    background-color: rgba(255, 255, 255, 0.2); \n"
 "    border: 2px solid white;  \n"
 "    border-radius: 10px;  \n"
@@ -133,7 +136,6 @@ class Ui_MainWindow(object):
 "\n"
 "QLabel {\n"
 "    color: white; \n"
-"    font-size: 14px;\n"
 "    font-weight: bold;\n"
 "}\n"
 "\n"
@@ -157,23 +159,25 @@ class Ui_MainWindow(object):
 "    background-color: #45a049;\n"
 "}\n"
 "")
-        self.correo_label_2 = QLabel(self.groupBox_2)
+        self.correo_label_2 = QLabel(self.groupBox_register)
         self.correo_label_2.setObjectName(u"correo_label_2")
         self.correo_label_2.setGeometry(QRect(50, 40, 181, 16))
-        self.correo_label_2.setFont(font1)
-        self.contrasena_label_2 = QLabel(self.groupBox_2)
+        font3 = QFont()
+        font3.setBold(True)
+        self.correo_label_2.setFont(font3)
+        self.contrasena_label_2 = QLabel(self.groupBox_register)
         self.contrasena_label_2.setObjectName(u"contrasena_label_2")
         self.contrasena_label_2.setGeometry(QRect(50, 110, 111, 16))
-        self.input_correo_2 = QLineEdit(self.groupBox_2)
+        self.input_correo_2 = QLineEdit(self.groupBox_register)
         self.input_correo_2.setObjectName(u"input_correo_2")
         self.input_correo_2.setGeometry(QRect(50, 70, 331, 31))
-        self.input_contrasena_2 = QLineEdit(self.groupBox_2)
+        self.input_contrasena_2 = QLineEdit(self.groupBox_register)
         self.input_contrasena_2.setObjectName(u"input_contrasena_2")
         self.input_contrasena_2.setGeometry(QRect(50, 140, 331, 31))
-        self.button_register = QPushButton(self.groupBox_2)
+        self.button_register = QPushButton(self.groupBox_register)
         self.button_register.setObjectName(u"button_register")
         self.button_register.setGeometry(QRect(170, 250, 75, 31))
-        self.login_enlace = QLabel(self.groupBox_2)
+        self.login_enlace = QLabel(self.groupBox_register)
         self.login_enlace.setObjectName(u"login_enlace")
         self.login_enlace.setGeometry(QRect(110, 290, 211, 20))
         self.login_enlace.setStyleSheet(u"QLabel {\n"
@@ -181,10 +185,10 @@ class Ui_MainWindow(object):
 "    text-decoration: underline;\n"
 "    cursor: pointer;\n"
 "}")
-        self.contrasena_label_3 = QLabel(self.groupBox_2)
+        self.contrasena_label_3 = QLabel(self.groupBox_register)
         self.contrasena_label_3.setObjectName(u"contrasena_label_3")
         self.contrasena_label_3.setGeometry(QRect(50, 180, 161, 16))
-        self.input_repeatContra = QLineEdit(self.groupBox_2)
+        self.input_repeatContra = QLineEdit(self.groupBox_register)
         self.input_repeatContra.setObjectName(u"input_repeatContra")
         self.input_repeatContra.setGeometry(QRect(50, 210, 331, 31))
         self.stackedWidget.addWidget(self.register_2)
@@ -201,12 +205,12 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.label_background.setText("")
-        self.groupBox.setTitle("")
+        self.groupBox_login.setTitle("")
         self.correo_label.setText(QCoreApplication.translate("MainWindow", u"Correo Electronico", None))
         self.contrasena_label.setText(QCoreApplication.translate("MainWindow", u"Contrase\u00f1a", None))
         self.button_login.setText(QCoreApplication.translate("MainWindow", u"Login", None))
         self.registro_enlace.setText(QCoreApplication.translate("MainWindow", u"\u00bfNo tienes una cuenta? Reg\u00edstrate aqu\u00ed", None))
-        self.groupBox_2.setTitle("")
+        self.groupBox_register.setTitle("")
         self.correo_label_2.setText(QCoreApplication.translate("MainWindow", u"Correo Electronico", None))
         self.contrasena_label_2.setText(QCoreApplication.translate("MainWindow", u"Contrase\u00f1a", None))
         self.button_register.setText(QCoreApplication.translate("MainWindow", u"Registrar", None))
--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import sys
 from PyQt6 import uic
 from PyQt6.QtWidgets import QMainWindow, QApplication, QMessageBox
+import pyrebase
+import requests
+import json
+
+
+
+
+#! Configuración de Pyrebase
+config = {
+    "apiKey": "AIzaSyBhn8Aabjpo3u2DkTsyYzjIvmRztP2t-jo",
+    "authDomain": "partyfunamigos.firebaseapp.com",
+    "databaseURL": "https://partyfunamigos-default-rtdb.firebaseio.com",
+    "projectId": "partyfunamigos",
+    "storageBucket": "partyfunamigos.firebasestorage.app",
+    "messagingSenderId": "353114155020",
+    "appId": "1:353114155020:web:17b507b32667922b682629",
+    "measurementId": "G-WXJ46P4RPZ"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 
 class SpaceRoom(QMainWindow):
     def __init__(self):
         super(SpaceRoom, self).__init__()
         uic.loadUi("juego.ui", self)
+
+        #! Titulo ventana principal
+        self.setWindowTitle("Programa")
 
         #! Establecer la página inicial ( índice 0 ,inicio de sesión)
         self.stackedWidget.setCurrentIndex(0)
@@ -26,12 +50,28 @@ class SpaceRoom(QMainWindow):
         if not correo or not contrasena:
             self.show_error("Por favor, ingrese todos los datos")
             return
+        
+
+        #* Logica para iniciar sesión
+        try:
+            user = auth.sign_in_with_email_and_password(correo, contrasena)
+            print(f"Login exitoso con: {correo}") # todo : esto debemos mostrar una pantalla de exito
+            # ! Esto realiza el cambio de pantalla a la ventana principal
+            #self.stackedWidget.setCurrentIndex(1)
+
+        except:
+            self.show_error("Correo o contraseña incorrectos")
 
         print(f"Login con: {correo}, {contrasena}") #Esto son logs para el cambio de pantalla para versi se hace bien
 
     def handle_register(self, event): # Este metodo es para cambiar a la pagina de registro
         self.stackedWidget.setCurrentIndex(1)
         print("Cambio de pantalla a registro") #Esto son logs para el cambio de pantalla para versi se hace bien
+
+
+    def handle_login_page(self, event): # Este metodo es para volver a la pagina de inicio de sesion
+        self.stackedWidget.setCurrentIndex(0)
+        print("Cambiando a la pagina de inicio de sesion") #Esto son logs para el cambio de pantalla para versi se hace bien
 
     def handle_register_action(self): # Este metodo es para registrar un usuario
         correo = self.input_correo_2.text().strip()
@@ -47,11 +87,15 @@ class SpaceRoom(QMainWindow):
             return
 
         # todo: En esta parte tenemos que anadir la logica del registro
+        try:
+            user = auth.create_user_with_email_and_password(correo, contrasena)
+            print(f"Registro exitoso con: {correo}") # todo : esto debemos mostrar una pantalla de exito
+            self.stackedWidget.setCurrentIndex(0)
+        except:
+            self.show_error("Error al registrar el usuario")
         print(f"Registro con:  {correo}, {contrasena}")
 
-    def handle_login_page(self, event): # Este metodo es para volver a la pagina de inicio de sesion
-        self.stackedWidget.setCurrentIndex(0)
-        print("Cambiando a la pagina de inicio de sesion") #Esto son logs para el cambio de pantalla para versi se hace bien
+  
 
     def show_error(self, message): # Este metodo es para mostrar un mensaje de error en la pantalla
         msg = QMessageBox()
@@ -65,3 +109,6 @@ if __name__ == '__main__':
     dia = SpaceRoom()
     dia.show()
     sys.exit(app.exec())
+
+
+    
