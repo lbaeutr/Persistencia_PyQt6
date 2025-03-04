d7fa920826c4a64f4eaa6e8b04ab7a37345c61ca
lbaeutr <lbaeutr@gmail.com>
2025-03-04 09:45:03 +0100

    Login primera parte

--- juego_ui.py
+++ juego_ui.py
@@ -16,7 +16,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QGroupBox, QLabel, QLineEdit,
-    QMainWindow, QPushButton, QSizePolicy, QWidget)
+    QMainWindow, QPushButton, QSizePolicy, QStackedWidget,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -36,9 +37,21 @@ class Ui_MainWindow(object):
         self.label_background.setMaximumSize(QSize(1200, 700))
         self.label_background.setPixmap(QPixmap(u"resources/images/background/backgroundLogin.png"))
         self.label_background.setScaledContents(True)
-        self.groupBox = QGroupBox(self.centralwidget)
+        self.stackedWidget = QStackedWidget(self.centralwidget)
+        self.stackedWidget.setObjectName(u"stackedWidget")
+        self.stackedWidget.setEnabled(True)
+        self.stackedWidget.setGeometry(QRect(0, 10, 1191, 671))
+        self.stackedWidget.setStyleSheet(u"QStackedWidget {\n"
+"    background: transparent;\n"
+"}")
+        self.login = QWidget()
+        self.login.setObjectName(u"login")
+        self.login.setStyleSheet(u"QWidget {\n"
+"    background: transparent;\n"
+"}")
+        self.groupBox = QGroupBox(self.login)
         self.groupBox.setObjectName(u"groupBox")
-        self.groupBox.setGeometry(QRect(390, 220, 431, 251))
+        self.groupBox.setGeometry(QRect(380, 210, 431, 281))
         font = QFont()
         font.setPointSize(26)
         self.groupBox.setFont(font)
@@ -93,9 +106,94 @@ class Ui_MainWindow(object):
         self.button_login = QPushButton(self.groupBox)
         self.button_login.setObjectName(u"button_login")
         self.button_login.setGeometry(QRect(170, 190, 75, 31))
+        self.registro_enlace = QLabel(self.groupBox)
+        self.registro_enlace.setObjectName(u"registro_enlace")
+        self.registro_enlace.setGeometry(QRect(80, 230, 271, 20))
+        self.registro_enlace.setStyleSheet(u"QLabel {\n"
+"    color: blue;\n"
+"    text-decoration: underline;\n"
+"    cursor: pointer;\n"
+"}")
+        self.stackedWidget.addWidget(self.login)
+        self.register_2 = QWidget()
+        self.register_2.setObjectName(u"register_2")
+        self.register_2.setStyleSheet(u"QWidget {\n"
+"    background: transparent;\n"
+"}")
+        self.groupBox_2 = QGroupBox(self.register_2)
+        self.groupBox_2.setObjectName(u"groupBox_2")
+        self.groupBox_2.setGeometry(QRect(380, 170, 431, 341))
+        self.groupBox_2.setFont(font)
+        self.groupBox_2.setStyleSheet(u"QGroupBox {\n"
+"    background-color: rgba(255, 255, 255, 0.2); \n"
+"    border: 2px solid white;  \n"
+"    border-radius: 10px;  \n"
+"    padding: 10px;\n"
+"}\n"
+"\n"
+"QLabel {\n"
+"    color: white; \n"
+"    font-size: 14px;\n"
+"    font-weight: bold;\n"
+"}\n"
+"\n"
+"QLineEdit {\n"
+"    background-color: rgba(255, 255, 255, 0.8);\n"
+"    border: 1px solid gray;\n"
+"    border-radius: 5px;\n"
+"    padding: 5px;\n"
+"    font-size: 14px;\n"
+"}\n"
+"\n"
+"QPushButton {\n"
+"    background-color: #4CAF50; \n"
+"    color: white;\n"
+"    border-radius: 5px;\n"
+"    padding: 5px;\n"
+"    font-size: 14px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: #45a049;\n"
+"}\n"
+"")
+        self.correo_label_2 = QLabel(self.groupBox_2)
+        self.correo_label_2.setObjectName(u"correo_label_2")
+        self.correo_label_2.setGeometry(QRect(50, 40, 181, 16))
+        self.correo_label_2.setFont(font1)
+        self.contrasena_label_2 = QLabel(self.groupBox_2)
+        self.contrasena_label_2.setObjectName(u"contrasena_label_2")
+        self.contrasena_label_2.setGeometry(QRect(50, 110, 111, 16))
+        self.input_correo_2 = QLineEdit(self.groupBox_2)
+        self.input_correo_2.setObjectName(u"input_correo_2")
+        self.input_correo_2.setGeometry(QRect(50, 70, 331, 31))
+        self.input_contrasena_2 = QLineEdit(self.groupBox_2)
+        self.input_contrasena_2.setObjectName(u"input_contrasena_2")
+        self.input_contrasena_2.setGeometry(QRect(50, 140, 331, 31))
+        self.button_register = QPushButton(self.groupBox_2)
+        self.button_register.setObjectName(u"button_register")
+        self.button_register.setGeometry(QRect(170, 250, 75, 31))
+        self.login_enlace = QLabel(self.groupBox_2)
+        self.login_enlace.setObjectName(u"login_enlace")
+        self.login_enlace.setGeometry(QRect(110, 290, 211, 20))
+        self.login_enlace.setStyleSheet(u"QLabel {\n"
+"    color: blue;\n"
+"    text-decoration: underline;\n"
+"    cursor: pointer;\n"
+"}")
+        self.contrasena_label_3 = QLabel(self.groupBox_2)
+        self.contrasena_label_3.setObjectName(u"contrasena_label_3")
+        self.contrasena_label_3.setGeometry(QRect(50, 180, 161, 16))
+        self.input_repeatContra = QLineEdit(self.groupBox_2)
+        self.input_repeatContra.setObjectName(u"input_repeatContra")
+        self.input_repeatContra.setGeometry(QRect(50, 210, 331, 31))
+        self.stackedWidget.addWidget(self.register_2)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
+
+        self.stackedWidget.setCurrentIndex(1)
+
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
@@ -107,5 +205,12 @@ class Ui_MainWindow(object):
         self.correo_label.setText(QCoreApplication.translate("MainWindow", u"Correo Electronico", None))
         self.contrasena_label.setText(QCoreApplication.translate("MainWindow", u"Contrase\u00f1a", None))
         self.button_login.setText(QCoreApplication.translate("MainWindow", u"Login", None))
+        self.registro_enlace.setText(QCoreApplication.translate("MainWindow", u"\u00bfNo tienes una cuenta? Reg\u00edstrate aqu\u00ed", None))
+        self.groupBox_2.setTitle("")
+        self.correo_label_2.setText(QCoreApplication.translate("MainWindow", u"Correo Electronico", None))
+        self.contrasena_label_2.setText(QCoreApplication.translate("MainWindow", u"Contrase\u00f1a", None))
+        self.button_register.setText(QCoreApplication.translate("MainWindow", u"Registrar", None))
+        self.login_enlace.setText(QCoreApplication.translate("MainWindow", u"\u00bfYa tienes cuenta? Inicia sesion", None))
+        self.contrasena_label_3.setText(QCoreApplication.translate("MainWindow", u"Repite contrase\u00f1a", None))
     # retranslateUi
 
--- main.py
+++ main.py
@@ -7,8 +7,17 @@ class SpaceRoom(QMainWindow):
         super(SpaceRoom, self).__init__()
         uic.loadUi("juego.ui", self)
 
-        # Conectar el botón de inicio de sesión
+        #! Establecer la página inicial ( índice 0 ,inicio de sesión)
+        self.stackedWidget.setCurrentIndex(0)
+
+        #! Boton de inicio de sesión
         self.button_login.clicked.connect(self.handle_login)
+        #! Conectar el enlace de registro
+        self.registro_enlace.mousePressEvent = self.handle_register
+        #!  Boton de registro
+        self.button_register.clicked.connect(self.handle_register_action)
+        #! Conectar el enlace de inicio de sesión en la página de registro
+        self.login_enlace.mousePressEvent = self.handle_login_page
 
     def handle_login(self):
         correo = self.input_correo.text().strip()
@@ -18,9 +27,33 @@ class SpaceRoom(QMainWindow):
             self.show_error("Por favor, ingrese todos los datos")
             return
 
-        print(f"Login con: {correo}, {contrasena}")
+        print(f"Login con: {correo}, {contrasena}") #Esto son logs para el cambio de pantalla para versi se hace bien
 
-    def show_error(self, message):
+    def handle_register(self, event): # Este metodo es para cambiar a la pagina de registro
+        self.stackedWidget.setCurrentIndex(1)
+        print("Cambio de pantalla a registro") #Esto son logs para el cambio de pantalla para versi se hace bien
+
+    def handle_register_action(self): # Este metodo es para registrar un usuario
+        correo = self.input_correo_2.text().strip()
+        contrasena = self.input_contrasena_2.text().strip()
+        confirmar_contrasena = self.input_repeatContra.text().strip()
+
+        if  not correo or not contrasena or not confirmar_contrasena:
+            self.show_error("Por favor, ingrese todos los datos")
+            return
+
+        if contrasena != confirmar_contrasena:
+            self.show_error("Las contraseñas no coinciden")
+            return
+
+        # todo: En esta parte tenemos que anadir la logica del registro
+        print(f"Registro con:  {correo}, {contrasena}")
+
+    def handle_login_page(self, event): # Este metodo es para volver a la pagina de inicio de sesion
+        self.stackedWidget.setCurrentIndex(0)
+        print("Cambiando a la pagina de inicio de sesion") #Esto son logs para el cambio de pantalla para versi se hace bien
+
+    def show_error(self, message): # Este metodo es para mostrar un mensaje de error en la pantalla
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Icon.Critical)
         msg.setWindowTitle("Error")
