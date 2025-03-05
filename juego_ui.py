5cfca1e7b8597ef20367ec9381ee657b3d13612b
RaulToledoProcopio <rtolpro@gmail.com>
2025-03-05 07:14:04 +0100

    PantallaJuego

--- juego_ui.py
+++ juego_ui.py
@@ -256,7 +256,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.stackedWidget.setCurrentIndex(2)
+        self.stackedWidget.setCurrentIndex(1)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
